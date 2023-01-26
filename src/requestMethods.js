b00f2b16a6a6b5972fe5de5113a0711f23404795
qerimidriton <48260103+qerimidriton@users.noreply.github.com>
2023-01-26 15:12:43 +0100

    Update requestMethods.js

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://foodorder-api.onrender.com/";
+const BASE_URL = "https://foodorder-api.onrender.com/api/";
 
 const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
 const currentUser = user && JSON.parse(user).currentUser;
